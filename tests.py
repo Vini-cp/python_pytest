1283e5e776a9cb29c46ed5909c15498f95f7833e
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-09-03 10:26:50 -0300

    Initial files

---
+++ tests.py
@@ -0,0 +1,35 @@
+# - Agent : 
+#   - modifier un attribut position
+#   - récupérer un attribut position
+#   - assigner un dictionnaire en tant qu'attributs
+
+# - Position :
+#   - modifier un attribut longitude_degrees
+#   - modifier un attribut latitude_degrees
+#   - modifier un attribut longitude_degrees avec une valeur supérieure à 180 renvoie une erreur. 
+#   - modifier un attribut latitide_degrees avec une valeur supérieure à 90 renvoie une erreur. 
+#   - récupérer une latitude
+#   - récupérer une longitude
+
+# - Zone :
+#   - trouver une zone qui contient une position
+#   - ajouter un habitant dans une zone
+#   - récupérer toutes les instances Zone (Zone.ZONES)
+#   - récupérer la densité de population d'une zone
+#   - récupérer l'agréabilité moyenne d'une zone
+
+# - AgreeablenessGraph :
+#   - récupérer un titre
+#   - récupérer x_label
+#   - récupérer y_label
+#   - récupérer xy_values sous forme de tuples
+#   - la première valeur de xy_values est la densité de population moyenne
+#   - la seconde valeur de xy_values est l'agréabilité moyenne
+
+# - IncomeGraph :
+#   - récupérer un titre
+#   - récupérer x_label
+#   - récupérer y_label
+#   - récupérer xy_values sous forme de tuples
+#   - la première valeur de xy_values est l'âge
+#   - la seconde valeur de xy_values est le revenu
